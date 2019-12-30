3e3a44099dad25a56ef7a19bee287daf6a250e45
Hajarak <kassimhajara13@gmail.com>
2019-12-30 16:05:38 +0100

    portal

---
+++ signup/photo.php
@@ -0,0 +1,45 @@
+<?php
+include "connect.php";
+
+if(isset($_POST['but_upload'])){
+ 
+  $name = $_FILES['file']['name'];
+  $target_dir = "upload/";
+  $target_file = $target_dir . basename($_FILES["file"]["name"]);
+
+  // Select file type
+  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+  // Valid file extensions
+  $extensions_arr = array("jpg","jpeg","png","gif");
+
+  // Check extension
+  if( in_array($imageFileType,$extensions_arr) ){
+ 
+     // Insert record
+     $query = "insert into images(name) values('".$name."')";
+     mysqli_query($conn,$query);
+  
+     // Upload file
+     move_uploaded_file($_FILES['file']['tmp_name'],$target_dir.$name);
+
+
+
+  }
+ 
+}
+
+ $sql = "select name from images where id=1";
+      $result = mysqli_query($conn,$sql);
+      $row = mysqli_fetch_array($result);
+
+      $image = $row['name'];
+      $image_src = "upload/".$image;
+?>
+
+<form method="post" action="../Dashboard/index.php" enctype='multipart/form-data'>
+  <input type='file' name='file' />
+  <input type='submit' value='Save name' name='but_upload'>
+  
+</form>
+
